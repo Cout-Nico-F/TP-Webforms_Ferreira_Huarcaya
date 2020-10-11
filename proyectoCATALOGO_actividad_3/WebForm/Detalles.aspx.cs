889f7803a6c15d5b40e43b965fb9a779fbab2b6d
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-10-11 20:22:32 -0300

    Metodo buscar

--- proyectoCATALOGO_actividad_3/WebForm/Detalles.aspx.cs
+++ proyectoCATALOGO_actividad_3/WebForm/Detalles.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Modelo;
+using Negocios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,31 @@ namespace WebForm
 {
     public partial class Detalles : System.Web.UI.Page
     {
+        public int idArticulo;
+        ArticulosNegocio negocio = null;
+        List<Articulo> listaArticulos = null;
+        Articulo articuloDetalle = null;
         protected void Page_Load(object sender, EventArgs e)
         {
+            negocio = new ArticulosNegocio();
+            idArticulo =Convert.ToInt32( Request.QueryString["itemArticulo"] );
+            listaArticulos = negocio.ListarArticulos();
 
+            articuloDetalle = Buscar(listaArticulos,idArticulo);
         }
+
+        private Articulo Buscar(List<Articulo> lista , Int32 id )
+        {
+            foreach (Articulo item in lista)
+            {
+                if (item.Id.Equals(id))
+                {
+                    articuloDetalle = item;
+                    return articuloDetalle;
+                }
+            }
+            return articuloDetalle;
+        }
+
     }
 }
